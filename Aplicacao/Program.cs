37ab2626190fd16c5639270b63e702031b641cc5
Gleryston Matos <glerystonmatos@yahoo.com.br>
2021-06-07 06:51:29 -0300

    Preparação do projeto para apresentação.

--- Aplicacao/Program.cs
+++ Aplicacao/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace Aplicacao
 {
@@ -13,15 +12,7 @@ namespace Aplicacao
             Console.WriteLine("Subtrair: {0}", Utilitarios.Subtrair(20, 5));
             Console.WriteLine("Dividir: {0}", Utilitarios.Dividir(10, 5));
             Console.WriteLine("");
-            Console.WriteLine("");            
-
-            CidadeService cidadeService = new CidadeService(new CidadeRepository());
-            Console.WriteLine("Exemplo de testes com Nunit e Moq:");
-            Console.WriteLine("Create: {0}", cidadeService.Create(new Cidade("Fortaleza")));
-            Console.WriteLine("Update: {0}", cidadeService.Update(new Cidade("Fortaleza")));
-            Console.WriteLine("Remove: {0}", cidadeService.Remove(new Cidade("Fortaleza")));
-            Console.WriteLine("GetAll Count: {0}", cidadeService.GetAll().ToList().Count);
-            Console.WriteLine("FindById: {0}", cidadeService.FindById(Guid.NewGuid()).Nome);
+            Console.WriteLine("");
         }
     }
 }
